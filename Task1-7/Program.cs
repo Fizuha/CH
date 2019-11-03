ce02f3da89c5b165c1144d90ac1f6776bd1772f9
Fizuha <57192015+Fizuha@users.noreply.github.com>
2019-11-03 08:46:33 +0200

    Add project files.

---
+++ Task1-7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task1_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string RUalp = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            string ENalp = "abcdefghijklmnopqrstuvwxyz";
+            char[] En = ENalp.ToCharArray(); //РУ Алфавит в масиве
+            char[] Ru = RUalp.ToCharArray(); //EN Алфавит в масиве
+            string n = Console.ReadLine();// Ввод строки шифрования
+            n=n.ToLower();
+            
+            char[] InStr = n.ToCharArray();// Входящая строка в масив
+            Console.WriteLine("Задайте смещение.");
+            int num=0, i, offset=Convert.ToInt32(Console.ReadLine());//Ввод смещения
+            for (i = 0; i < n.Length; i++)
+            {
+                num = RUalp.IndexOf(InStr[i]);
+                if (num < 0)
+                {
+                    num = ENalp.IndexOf(InStr[i]);
+                    if (num + offset > ENalp.Length) num -= ENalp.Length;
+                    Console.Write(En[num + offset]);
+                }
+                else
+                {
+                    if (num + offset > RUalp.Length) num -= RUalp.Length;
+                    Console.Write(Ru[num + offset]);
+                }
+            }
+        }
+    }
+}
